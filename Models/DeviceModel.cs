fb3bb1e24fad94ea5e5b4bfc09ab64422c0aaccd
Vgogichashvili <vgogichashvili444@gmail.com>
2023-02-01 17:16:56 +0400

    changes

--- Models/DeviceModel.cs
+++ Models/DeviceModel.cs
@@ -2,9 +2,20 @@
 
 public class DeviceModel
 {
-    public int Id { get; set; }
-    public long DeviceId { get; set;} 
+    public int ID { get; set; }
+    public string? STARTBYTE { get; set;}
+    public long IMEI { get; set;}
+    public string? DATE  { get; set; }
+    public string? ADMISSION_LEVEL { get; set; }
+    public  string? PING { get; set; }
+    public string? DOOR_STATUS { get; set; }
+    public string? DOOR_CLOSE { get; set; }
+    public string? DOOR_OPEN { get; set; }
+    public string? RIGHT_DOOR { get; set; }
+    public string? LEFT_DOOR { get; set; }
+    public DateTime RECORDING_TIME { get; set; } = DateTime.Now;
+    
+    public long? DeviceId { get; set;} 
     public string? DeviceName { get; set; }
-    public long Imei { get; set; }
 
 }
--- Models/ListenerModel.cs
+++ Models/ListenerModel.cs
@@ -3,15 +3,18 @@
 public class ListenerModel
 {
     
-    public int Id { get; set; }
+    public int ID { get; set; }
     public string? STARTBYTE { get; set;}
     public long IMEI { get; set;}
     public string? DATE  { get; set; }
     public string? ADMISSION_LEVEL { get; set; }
+    public  string? PING { get; set; }
     public string? DOOR_STATUS { get; set; }
     public string? DOOR_CLOSE { get; set; }
     public string? DOOR_OPEN { get; set; }
-    public string? LEFT_DOOR { get; set; }
     public string? RIGHT_DOOR { get; set; }
+    public string? LEFT_DOOR { get; set; }
+    public DateTime RECORDING_TIME { get; set; } = DateTime.Now;
+  
     
 }
